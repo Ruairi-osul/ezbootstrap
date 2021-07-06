7d385fbe31c72d9c04f86ea8e3f29c2e2124bb92
Ruairi-Osul <rorylikesmusic@gmail.com>
2021-07-06 17:15:28 -0400

    tidied up setup py

--- setup.py
+++ setup.py
@@ -4,17 +4,15 @@ with open("requirements.txt") as f:
     reqs = f.read().splitlines()
 
 description = "Bootstrap statistics package."
-long_description = ""
 setup(
     name="ezbootstrap",
     description=description,
-    long_description=long_description,
     version="0.0.1",
     url="https://github.com/Ruairi-osul/ezbootstrap",
     author="Ruairi O'Sullivan",
     author_email="ruairi.osullivan.work@gmail.com",
     license="GNU GPLv3",
-    keywords="statistics scientific-computing data data-science",
+    keywords="statistics scientific-computing data data-science bootstrap",
     project_urls={"Source": "https://github.com/Ruairi-osul/ezbootstrap"},
     packages=find_packages(),
     python_requires=">=3.3",
